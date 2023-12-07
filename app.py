f61a21f5293da99e4ffa18be7894dcc45ec0b0e3
Gautam-10137 <pahwagautam786@gmail.com>
2023-12-07 17:23:48 +0530

    updated

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, request, jsonify
 import requests
 from transformers import pipeline
 import os
+
 app = Flask(__name__)
 
 LANGUAGE_TOOL_API_URL = 'https://api.languagetool.org/v2/check'
+
+# Specify the model name and revision
+model_name = "sshleifer/distilbart-cnn-12-6"
+revision = "a4f8f3e"
+
+# Create the summarization pipeline with explicit model name and revision
+summarizer = pipeline('summarization', model=model_name, revision=revision)
 
 @app.route('/summarize', methods=['POST'])
 def summarize_and_correct():
@@ -12,8 +20,7 @@ def summarize_and_correct():
         data = request.get_json()
         text = data['text']
 
-        # Perform text summarization using the Bart model from transformers
-        summarizer = pipeline('summarization')
+        # Perform text summarization using the specified Bart model
         summarized_text = summarizer(text, max_length=150, min_length=50, length_penalty=2.0, num_beams=4, early_stopping=True)[0]['summary_text']
 
         # Perform grammar correction using LanguageTool API
@@ -41,5 +48,3 @@ def summarize_and_correct():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
